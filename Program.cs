b5b2d4dc2679d857bc39352ed2fa28f56cc96916
Thyago <thyagothyago600@gmail.com>
2025-11-28 21:35:15 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Tirar a media");
+Console.WriteLine("Primeira Nota :");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("segunda Nota :");
+int n2 = int.Parse(Console.ReadLine());
+Console.WriteLine("terceira Nota :");
+int n3 = int.Parse(Console.ReadLine());
+int media = (n1 + n2 + n3 ) / 3 ;
+Console.WriteLine($"A media é {media}");
